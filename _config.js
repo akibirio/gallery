4f8da89b55f06db12f12b7496e6ddb1d68431a2c
ayubkibirio <ayubkibirio@gmail.com>
2022-07-20 12:52:11 +0300

    working with master

---
+++ _config.js
@@ -0,0 +1,7 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://sprintech:sprintech@sprintech.qf4jkk2.mongodb.net/?retryWrites=true&w=majority',
+}
+module.exports = config;
